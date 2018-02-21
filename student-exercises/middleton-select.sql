e1a7935b0c0316a9e66c292e013e11cd48dfd02e
Alex <alexmiddleton@outlook.com>
2018-02-21 15:17:21 -0500

    restricting of rows returned

--- student-exercises/middleton-select.sql
+++ student-exercises/middleton-select.sql
@@ -1,1 +1,6 @@
 SELECT id, name, genre from band;
+
+SELECT * FROM individual WHERE LastName = 'Jennings';
+SELECT ID, LastName from individual WHERE DeceasedDate IS NOT NULL;
+SELECT ID LastName, BirthDate from individual where YEAR(BirthDate) > 1940;
+SELECT * FROM individual where ID in (1,3,5,7,19);
