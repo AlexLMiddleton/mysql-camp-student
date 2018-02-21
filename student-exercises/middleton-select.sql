28fdb12364493544b399a4e47ee4110a9ac739ed
Alex <alexmiddleton@outlook.com>
2018-02-21 15:25:46 -0500

    translating requirements to SQL

--- student-exercises/middleton-select.sql
+++ student-exercises/middleton-select.sql
@@ -4,3 +4,6 @@ SELECT * FROM individual WHERE LastName = 'Jennings';
 SELECT ID, LastName from individual WHERE DeceasedDate IS NOT NULL;
 SELECT ID LastName, BirthDate from individual where YEAR(BirthDate) > 1940;
 SELECT * FROM individual where ID in (1,3,5,7,19);
+
+select name from band where genre = 'Alternative' and isTogether = 0;
+select id, name from band where id = 4;
